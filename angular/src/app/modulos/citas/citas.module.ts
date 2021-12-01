564d9a55063aa625cdf3273af78e0b4952917b92
nealos8907 <nestor_ospina_8907@hotmail.com>
2021-12-01 17:22:53 -0500

    adecuación de rutas y algunas vistas

--- angular/src/app/modulos/citas/citas.module.ts
+++ angular/src/app/modulos/citas/citas.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CitasRoutingModule } from './citas-routing.module';
+import { AsignarCitasComponent } from './asignar-citas/asignar-citas.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AsignarCitasComponent
+  ],
   imports: [
     CommonModule,
     CitasRoutingModule
