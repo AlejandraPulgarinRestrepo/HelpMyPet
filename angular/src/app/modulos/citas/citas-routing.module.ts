4e2a3415a7362991d7738bd8d04498fdd2b496ac
nealos8907 <nestor_ospina_8907@hotmail.com>
2021-12-01 17:38:28 -0500

    Se adicioona vistas de registro de citas

--- angular/src/app/modulos/citas/citas-routing.module.ts
+++ angular/src/app/modulos/citas/citas-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignarCitasComponent } from './asignar-citas/asignar-citas.component';
+import { RegistrarCitaComponent } from './registro-citas/registrar-cita/registrar-cita.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"asignar-cita",
+    component: AsignarCitasComponent
+    
+  },
+  {
+    path: "registrar-cita",
+    component: RegistrarCitaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- angular/src/app/modulos/citas/citas.module.ts
+++ angular/src/app/modulos/citas/citas.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CitasRoutingModule } from './citas-routing.module';
 import { AsignarCitasComponent } from './asignar-citas/asignar-citas.component';
+import { RegistrarCitaComponent } from './registro-citas/registrar-cita/registrar-cita.component';
+import { BuscarCitaComponent } from './registro-citas/buscar-cita/buscar-cita.component';
+import { ActualizarCitaComponent } from './registro-citas/actualizar-cita/actualizar-cita.component';
+import { EliminarCitaComponent } from './registro-citas/eliminar-cita/eliminar-cita.component';
 
 
 @NgModule({
   declarations: [
-    AsignarCitasComponent
+    AsignarCitasComponent,
+    RegistrarCitaComponent,
+    BuscarCitaComponent,
+    ActualizarCitaComponent,
+    EliminarCitaComponent
   ],
   imports: [
     CommonModule,
